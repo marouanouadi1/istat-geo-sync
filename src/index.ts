12b869484d3bd99891324322f55ada531eda3f2b
Marouan <ouadimarouan@gmail.com>
2025-09-21 15:05:51 +0200

    feat: add command export

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Command, Option, Argument } from "commander";
+import { version } from "../package.json";
+
+const program = new Command();
+
+program
+  .name("istat-geo-sync")
+  .description(
+    "Sync/export ISTAT geographic datasets (Regions/Provinces/Municipalities)"
+  )
+  .version(version);
+
+program
+  .command("export")
+  .description("Export normalized datasets to files")
+  .addArgument(
+    new Argument("<entity>", "Entity to export").choices([
+      "regions",
+      "provinces",
+      "municipalities",
+      "all",
+    ])
+  )
+  .addOption(
+    new Option("-f, --format <format...>", "one or more output formats")
+      .choices(["csv", "json"])
+      .makeOptionMandatory(true)
+  )
+  .option("-o, --out <dir>", "output directory", "out")
+  .option(
+    "--filename <pattern>",
+    "output filename pattern; placeholders: {entity},{date},{ext}",
+    "{date}-{entity}.{ext}"
+  );
+
+program.parse();
